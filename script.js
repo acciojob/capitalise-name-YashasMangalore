29a096c5c6768037cb4344978695d05de2c1308a
YashasMangalore <129035779+YashasMangalore@users.noreply.github.com>
2024-08-26 09:27:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.getElementById("fname").onmouseout=function()
+												{
+													mouseout()
+												};
+
+function mouseout()
+{
+	const temp=document.getElementById("fname");
+	temp.value=temp.value.toUpperCase();
+}
